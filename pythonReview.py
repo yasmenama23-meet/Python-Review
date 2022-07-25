df9fb427ee59a70a130d83cc269e0b31da2762ce
yasmena masalha <109899488+yasmenama23-meet@users.noreply.github.com>
2022-07-25 12:39:18 +0300

    end

--- pythonReview.py
+++ pythonReview.py
@@ -1,1 +1,17 @@
-def create_youtube_video():
+def create_youtube_video(title,description,likes, dilikes,comments):
+	dictionary1 = {"title":title , "description":description ,"likes":likes,"dislikes":dilikes,"comments":comments}
+return dictionary1
+
+def like(dictionary1):
+    if"likes" in dictionary1:
+	   dictionary1["likes"]+=
+	return dictionary1
+
+def add_comment(youtube,username,comments_text):
+	youtubevideo["comments"] [username]=comments_text
+	return youtubevideo
+
+ dictionary2=create_youtube_video("banna cake","plaplapal")
+ for i in range(495):
+ 	 like(dictionary2)
+   print(dictionary2)
